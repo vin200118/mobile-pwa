133fe327e47b20ee7a419088f3dcb66b34c0c319
vin200118 <padekarvinayak5@gmail.com>
2019-10-15 22:29:29 +0530

    first commit

---
+++ precache-manifest.913e1e1b2e4ef9c55d30cac7766ce263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27a40d763a92b46a7f4d297ad50c804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f8804984cad082bae4",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "177c0ee6d9bb3adc36b0",
+    "url": "/static/js/2.61999575.chunk.js"
+  },
+  {
+    "revision": "a8f8804984cad082bae4",
+    "url": "/static/js/main.1baadfac.chunk.js"
+  },
+  {
+    "revision": "01f238b4c1204b49948c",
+    "url": "/static/js/runtime-main.e55af994.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
